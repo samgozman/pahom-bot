d35abd545b8a6e9122e715580410b534dcc39bac
samgozman <sam@gozman.space>
2019-06-22 13:01:30 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='pahom',
+    version='0.1.4',
+    packages=['pahom'],
+    install_requires=[
+        'python-telegram-bot',
+        'apiai',
+        'markovify',
+        'emoji',
+        'PySocks'
+    ],
+    # data_files=[('pahom', ['pahom/shiza.txt', 'pahom/markov.txt'])],
+    include_package_data=True,
+    zip_safe=False
+    # long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    # python3 setup.py sdist
+    # to setup: pip3 install pahom-0.1.tar.gzc
+    # to update: pip3 install pahom-0.1.tar.gzc --upgrade
+    # to run this shit use:
+    # python3 -m pahom.__init__
+)
